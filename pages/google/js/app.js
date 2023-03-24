9d03e0e7975bcf729a38c8d26e494fdb5d2a5320
Diego Franco Giusti Modesto da Silva <diegogiusti08@gmail.com>
2023-03-24 19:03:10 -0300

    refatoração do código

--- pages/google/js/app.js
+++ pages/google/js/app.js
@@ -3,26 +3,32 @@ const navIconsHeader = document.querySelectorAll('.l-header__content')
 const popupApps = document.querySelector('.popup--apps')
 const popupAccount = document.querySelector('.popup--account')
 
-console.log(navIconsHeader)
+const closePopups = ({target}) => {
+    const isWrapper = target.className === "popup-wrapper"
 
-popupWrapper.addEventListener('click', event => {
-    if(event.target.className === "popup-wrapper"){
+    if(isWrapper){
         popupWrapper.style.position = 'static'
         popupApps.style.display = 'none'
         popupAccount.style.display = 'none'
     } 
-})
+}
 
-navIconsHeader[1].addEventListener('click', event => {
-    const clickedElement = event.target
-   
-   if(clickedElement.className === 'apps' || clickedElement.classList[1] === 'link--applications'){
+const openPopups = ({target}) => {
+    const clickedElement = target
+    const isAppsPopup = clickedElement.className === 'apps' || clickedElement.classList[1] === 'link--applications'
+    const isAccountPopup = clickedElement.className === 'avatar' || clickedElement.classList[1] === 'link--avatar'
+
+   if(isAppsPopup){
         popupApps.style.display = 'block'
         popupAccount.style.display = 'none'
         popupWrapper.style.position = 'absolute'
-   }else if(clickedElement.className === 'avatar' || clickedElement.classList[1] === 'link--avatar'){
+        
+   }else if(isAccountPopup){
         popupAccount.style.display = 'block'
         popupApps.style.display = 'none'
         popupWrapper.style.position = 'absolute'
    }
-})
+}
+
+popupWrapper.addEventListener('click', closePopups)
+navIconsHeader[1].addEventListener('click', openPopups)
